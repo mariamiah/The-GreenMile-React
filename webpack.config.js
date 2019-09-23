6f0dfab24ace5411c96384f31f7e722de260c34a
mariah <mariah.kakande@gmail.com>
2019-09-23 21:38:28 +0300

    ft(setup): set up the react application
    - install dependencies
    - set up webpack
    - set up babel
    - add eslint
    - set up prettier
    
    [Delivers #168701984]

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'},
+        devServer: {
+        contentBase: './dist',  },  
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader', 'eslint-loader']
+                },
+                {
+                    test: /\.less$/,
+                    use: [
+                        'style-loader',      'css-loader',      'less-loader', 
+                    ]
+                }
+            ]  
+        },
+    };
